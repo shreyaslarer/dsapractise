dc2f39729f05086939789564c6d4f86d3836a800
Shreyas L Arer <shreyalshreyasl5@gmail.com>
2026-08-06 15:20:21 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,38 +2,47 @@ class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
 
-        // Base Case:
-        // If the array is empty or contains only one element,
-        // no shifting is required.
-        if (arr.empty() || arr.size() == 1) {
-            return;
-        }
-
-        // Step 1: Find the index of the first zero.
-        int j = -1;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] == 0) {
-                j = i;
-                break;
-            }
-        }
-
-        // Step 2: If there are no zeros in the array,
-        // the array is already in the desired state.
-        if (j == -1) {
-            return;
-        }
-
-        // Step 3: Traverse the remaining array.
-        // Whenever a non-zero element is found,
-        // swap it with the zero at index j.
-        for (int i = j + 1; i < arr.size(); i++) {
-            if (arr[i] != 0) {
-                swap(arr[i], arr[j]);
-
-                // Move j to the next zero position.
+        int j =  0;
+        for(int i =0; i<arr.size(); i++){
+            if (arr[i]!=0){
+                swap(arr[i],arr[j]);
                 j++;
             }
         }
+
+    //     // Base Case:
+    //     // If the array is empty or contains only one element,
+    //     // no shifting is required.
+    //     if (arr.empty() || arr.size() == 1) {
+    //         return;
+    //     }
+
+    //     // Step 1: Find the index of the first zero.
+    //     int j = -1;
+    //     for (int i = 0; i < arr.size(); i++) {
+    //         if (arr[i] == 0) {
+    //             j = i;
+    //             break;
+    //         }
+    //     }
+
+    //     // Step 2: If there are no zeros in the array,
+    //     // the array is already in the desired state.
+    //     if (j == -1) {
+    //         return;
+    //     }
+
+    //     // Step 3: Traverse the remaining array.
+    //     // Whenever a non-zero element is found,
+    //     // swap it with the zero at index j.
+    //     for (int i = j + 1; i < arr.size(); i++) {
+    //         if (arr[i] != 0) {
+    //             swap(arr[i], arr[j]);
+
+    //             // Move j to the next zero position.
+    //             j++;
+    //         }
+    //     }
+    // }
     }
 };
